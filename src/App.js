903ed467b50c7d86b7f9c389bc4fd59c1c06719b
Andrew Petrosky <18454946+ajpetrosky@users.noreply.github.com>
2023-01-28 12:37:22 -0500

    feat: minor updates

--- src/App.js
+++ src/App.js
@@ -5,17 +5,19 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <svg width="100" height="100">
+          <circle cx="50" cy="50" r="40" stroke="green" stroke-width="4" fill="green" />
+        </svg> 
         <p>
           AP's website coming soon.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://media.giphy.com/media/GxQABXQhII7cY/giphy.gif?cid=ecf05e47tmv2zf7o2pnpjx083czpazk64a67w829p9eo8wru&rid=giphy.gif&ct=g"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Go here instead, for now.
         </a>
       </header>
     </div>
